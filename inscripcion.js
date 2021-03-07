2fedd0c47051350e3365dcfc3a3af48e14da4255
alejo82 <alejandro.bohorquez61@correo.tdea.edu.co>
2021-03-07 18:40:55 -0500

    first commit

--- inscripcion.js
+++ inscripcion.js
@@ -1,4 +1,6 @@
 let { cursos } = require('./listar');
+const express = require('express')
+const app = express()
 
 let fs = require('fs');
 
@@ -22,21 +24,24 @@ let argv = require('yargs')
     .command('inscribir', 'Comandos para matricular', opciones)
     .argv;
 
-
 let inscribirCurso = () => {
 
     if (argv.id == 1 || argv.id == 2 || argv.id == 3) {
-        let salida = `El estudiante ${argv.nombre} de cedula: ${argv.cedula} se ha matriculado en el curso ${cursos[argv.id - 1].nombreC} con una duracion de ${cursos[argv.id - 1].duracion} y un valor de ${cursos[argv.id - 1].valor}`;
-        fs.writeFile('documentoCursos.txt', salida, (err) => {
-            if (err) throw (err);
-            console.log('Usted fue inscrito en el curso!!!');
-        })
+        salida = `El estudiante ${argv.nombre} de cedula: ${argv.cedula} se ha matriculado en el curso ${cursos[argv.id - 1].nombreC} con una duracion de ${cursos[argv.id - 1].duracion} y un valor de ${cursos[argv.id - 1].valor}`;
+        
     } else {
-        console.log('El codigo que ingreso no es correcto!!!');
+           salida= ('El codigo que ingreso no es correcto!!!');
     }
 
 
 
 }
+
+app.get('/', function (req, res) {
+    res.send(salida)
+  })
+   
+  app.listen(3000)
+  
 
 inscribirCurso();
